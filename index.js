d7c5f0354b0e9aa27a8c009682d899580e09c072
leolace <lgonsalez02@gmail.com>
2024-08-27 00:52:20 -0300

    feat: add line numbering, new line when press Enter key, backspace and more

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 "use strict";
 const container = document.getElementById("container");
+const numbers = document.getElementById("numbers");
 if (!container)
     throw new Error("Container root not found");
 const WIDTH = 1000;
@@ -11,23 +12,89 @@ let activeInput = null;
 const rows = Array(ROWS_COUNT).fill(Array(COLS_COUNT).fill(undefined));
 function createInput(row) {
     const input = document.createElement("input");
+    const textElement = row.getElementsByTagName("p")[0] || document.createElement("p");
+    input.value = textElement.textContent || "";
     input.style.height = "0";
     input.style.width = "0";
     activeInput = input;
     input.addEventListener("input", (e) => {
-        if (e instanceof InputEvent) {
-            const { data, inputType } = e;
-            console.log({ data, inputType });
-            switch (inputType) {
-                case "insertText":
-                    row.append(data);
-                    break;
-                case "deleteContentBackward":
-                    break;
-            }
+        if (!(e instanceof InputEvent))
+            return;
+        row.appendChild(textElement);
+        const { data, inputType } = e;
+        switch (inputType) {
+            case "insertText":
+                textElement.textContent = textElement.textContent?.concat(data) || null;
+                row.replaceChild(textElement, textElement);
+                break;
+            case "deleteContentBackward":
+                textElement.textContent = textElement.textContent?.slice(0, textElement.textContent?.length - 1) || null;
+                row.replaceChild(textElement, textElement);
+                break;
+        }
+    });
+    input.addEventListener("keydown", (e) => {
+        if (!(e instanceof KeyboardEvent))
+            return;
+        console.log(e);
+        switch (e.code) {
+            case "Backspace":
+                if (!Boolean(textElement.textContent) && row.contains(textElement))
+                    deleteRow(row);
+                break;
+            case "Enter":
+                let nextElement = row.nextElementSibling;
+                if (!nextElement) {
+                    nextElement = createRow();
+                    container?.append(nextElement);
+                }
+                if (nextElement instanceof HTMLPreElement)
+                    nextElement.click();
+                break;
         }
     });
     return input;
+}
+function deleteRow(row) {
+    if (!container || !numbers)
+        throw new Error("Container root not defined");
+    container.removeChild(row);
+    const lastChildNumber = numbers.lastChild;
+    if (lastChildNumber)
+        numbers.removeChild(numbers.lastChild);
+    if (container.lastElementChild && container.lastElementChild instanceof HTMLPreElement)
+        container.lastElementChild.click();
+}
+function createRow() {
+    if (!container)
+        throw new Error("Container root not defined");
+    const index = container.childElementCount.toString();
+    const row = document.createElement("pre");
+    const n = document.createElement("span");
+    row.id = "row";
+    row.dataset.nth = index;
+    n.textContent = index;
+    numbers?.appendChild(n);
+    row.addEventListener("click", (e) => {
+        if (!(e.currentTarget instanceof HTMLPreElement))
+            return;
+        if (!activeRow) {
+            activeRow = row;
+            createInput(activeRow);
+        }
+        if (e.currentTarget.dataset.nth !== activeRow.dataset.nth) {
+            removeInput(activeInput, activeRow);
+            activeRow.classList.remove("active");
+            activeRow = row;
+            createInput(activeRow);
+        }
+        if (!activeInput)
+            return;
+        row.classList.add("active");
+        row.appendChild(activeInput);
+        activeInput.focus();
+    });
+    return row;
 }
 function removeInput(input, row) {
     if (!input)
@@ -37,36 +104,11 @@ function removeInput(input, row) {
     activeInput = null;
 }
 (() => {
-    const rows = [];
+    const rows = [{ text: "" }];
     container.style.height = HEIGHT.toString();
     container.style.width = WIDTH.toString();
-    for (let i = 0; i < ROWS_COUNT; i++) {
-        const row = document.createElement("pre");
-        row.id = "row";
-        row.dataset.nth = i.toString();
-        row.addEventListener("click", (e) => {
-            if (e.currentTarget instanceof HTMLPreElement) {
-                if (!activeRow) {
-                    activeRow = row;
-                    const input = createInput(activeRow);
-                    row.appendChild(input);
-                    input.focus();
-                }
-                else if (e.currentTarget.dataset.nth !== activeRow.dataset.nth) {
-                    removeInput(activeInput, activeRow);
-                    activeRow = row;
-                    const input = createInput(activeRow);
-                    row.appendChild(input);
-                    input.focus();
-                }
-            }
-        });
+    for (let i = 0; i < rows.length; i++) {
+        const row = createRow();
         container.appendChild(row);
     }
 })();
-const a = {
-    name: "leonardo",
-    getName() {
-        return this.name;
-    },
-};
